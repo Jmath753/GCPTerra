e0339aab09d8aeb6a40b954c5423c03368388ef4
Jeremy Mathew <51455891+Jmath753@users.noreply.github.com>
2021-02-26 12:03:21 -0500

    Delete GCP1 directory

--- GCP1/output.tf
+++
@@ -1,8 +0,0 @@
-output "Internal_IP" {
-value = google_compute_instance.myvm1.network_interface.0.network_ip
-}
-
-output "External_IP" {
-value = google_compute_instance.myvm1.network_interface.0.access_config.0.nat_ip
-}
-    
--- GCP1/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "google" {
-  credentials = "${file("test-30068-3f3a632a12cf.json")}"
-  project     = "test-30068"
-  region      = "us-central1"
-  zone        = "us-central1-c"
-}
